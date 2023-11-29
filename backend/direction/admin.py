71f74cda945410947f32c8512ac17c9841b8f9de
number92 <lexa_rav@list.ru>
2023-11-29 15:00:02 +0300

    add abstractuser and settings for postgres

--- backend/direction/admin.py
+++ backend/direction/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "username",
+        "id",
+        "email",
+        "password",
+        "first_name",
+        "last_name",
+    )
+    list_editable = ("password", "email")
+    list_filter = ("username", "email")
+    search_fields = ("username", "email")
