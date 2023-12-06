1ef128c8a04d77c86d05a3258d2c09dc4b4fe563
number92 <rav.92@list.ru>
2023-12-06 15:28:41 +0300

    close branch

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import get_object_or_404
 
 from rest_framework.views import APIView
 from rest_framework import status
-
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from api.serializers import FirstStepSerializer, UserGradeSerializer
@@ -29,6 +28,7 @@ class FirstStepView(APIView):
     def post(self, request):
         context = request.data
         context["user"] = request.user.id
+
         serializer = UserGradeSerializer(data=context)
 
         serializer.is_valid(raise_exception=True)
